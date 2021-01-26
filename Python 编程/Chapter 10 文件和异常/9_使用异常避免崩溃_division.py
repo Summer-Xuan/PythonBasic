183881706361c1b3e806017361c8d97a4b816b72
xuanhsh <xuanhsh@inspur.com>
2021-01-26 13:44:29 +0800

    20210126

--- Python 编程/Chapter 10 文件和异常/9_使用异常避免崩溃_division.py
+++ Python 编程/Chapter 10 文件和异常/9_使用异常避免崩溃_division.py
@@ -1,6 +1,10 @@
 print("Give me two numbers, and I'll divide them.")
 print("Enter 'q' to quit.")
 
+"""
+通过将可能引发错误的代码放在try-except代码块中，可提高这个程序抵御错误的能力.
+依赖于try代码块成功执行的代码都放在else代码块中
+"""
 while True:
     first_number = input("\nFirst number:")
     if first_number == 'q':
@@ -10,7 +14,7 @@ while True:
         break
     try:
         answer = int(first_number)/int(second_number)
-    except ZeroDivisionError:
+    except :  # ZeroDivisionError
         print("You can't divide by 0!")
     else:
         print(answer)
