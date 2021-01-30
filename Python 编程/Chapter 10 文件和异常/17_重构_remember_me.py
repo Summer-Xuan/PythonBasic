87f7f04f458739b06da536616dff4aa4098603ac
xuanhsh <xuanhsh@inspur.com>
2021-01-30 15:49:54 +0800

    202101301549

--- Python 编程/Chapter 10 文件和异常/17_重构_remember_me.py
+++ Python 编程/Chapter 10 文件和异常/17_重构_remember_me.py
@@ -3,18 +3,26 @@
 重构让代码更清晰、更易于理解、更容易扩展。
 """
 import json
-filename = "demo/username.json"
+
 def get_stored_username():
+    """ 如果存储了用户名，就获取它 """
+    filename = "username.json"
     try:
         with open(filename) as f_obj:
             username = json.load(f_obj)
-            
-
-
-
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
 def get_new_username():
-    pass
+    """ 提示用户输入用户名 """
+    username = input("What is your name?")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+        return username
+
 
 def greet_user():
     """问候用户，并指出其姓名"""
--- Python 编程/Chapter11 测试代码/2_测试函数_names.py
+++ Python 编程/Chapter11 测试代码/2_测试函数_names.py
@@ -1,15 +1,14 @@
 from name_function import get_formatted_name
+import unittest
+
+class NamesTestCase(unittest.TestCase):
+    "测试name_function.py"
+    def test_first_last_name(self):
+        """能够正确地处理像Janis Joplin这样地姓名吗？"""
+        formatted_time = get_formatted_name("janis", "joplin")
+        self.assertEqual(formatted_time, "Janis Joplin")
 
 
-"""编写一个使用这个函数的程序。程序names.py让用户输入名和姓，并显示整洁的全名"""
-print("Enter 'q' at any time to quit")
-while True:
-    first = input("\nPlease give me a first time")
-    if first == 'q':
-        break
-    last = input("Please give me a last time")
-    if last == 'q':
-        break
-    formatted_time = get_formatted_name(first, last)
-    print("\tNeatly formatted name:" + formatted_time + '.')
+    def test_first_last_middle
+unittest.main()
 
